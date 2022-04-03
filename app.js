8d2d4420c5909c07d1ad888bd2d9ca83c63454c0
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-03 16:11:34 -0700

    Practice debugging

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const app = express();
 const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 
-const app = express();
-const db = require('./config/keys').mongoURI;
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Bye World"));
+app.get("/", (req, res) => {
+    res.send("Bye World")
+});
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
