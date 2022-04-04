bc976be9401a50194dc3002814bd29f46ba27628
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-03 18:40:20 -0700

    Work on tweets route

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const User = require('./models/User');
-const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
-    
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     const user = new User({
@@ -24,9 +22,17 @@ app.get("/", (req, res) => {
     user.save()
     res.send("Bye World")
 });
+
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const User = require('./models/User');
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
