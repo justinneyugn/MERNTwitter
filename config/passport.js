287409a0f3e9dadb24c484b1dbc74174c6978825
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-03 20:15:32 -0700

    Add all routes for tweets

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jewt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('./keys');
