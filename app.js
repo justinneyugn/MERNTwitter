81d777296b8b11cc69452c63b604ce51dbe025ae
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-03 17:17:37 -0700

    Add User Auth

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 const bodyParser = require('body-parser');
-
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
+    
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
     res.send("Bye World")
 });
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
